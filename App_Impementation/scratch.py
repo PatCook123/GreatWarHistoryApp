afbd198bd2e2df909ceb420cf0808f352d24b38d
PatCook123 <cookpat@oregonstate.edu>
2022-11-21 19:00:34 -0700

    nations page implemented along with guides for all pages. sources to come.

---
+++ App_Impementation/scratch.py
@@ -0,0 +1,20 @@
+nations = {
+    "France": {
+        "name": "France",
+        "date_joined": "July 23rd, 1914",
+        "alignment": "Allies / Triple Entente",
+        "flag": "/images/Flag_of_France_(lighter_variant).svg",
+        "casualties": "1.4 million dead, 4.2 million wounded",
+        "participation": "France was one of the main participants in the outbreak and fighting of World War I.  In fact, France was one of the main Allied Powers, alongside Britain and Russia.  As well, much of the fighting along the trenches of the Western Front occurred in northern France."
+    },
+    "Austria Hungary": {
+        "name": "Austro-Hungarian Empire",
+        "date_joined": "July 23rd, 1914",
+        "alignment": "Central Powers / Triple Alliance",
+        "flag": "Flag_of_Austria-Hungary_(1869-1918).svg",
+        "casualties": "1.7 - 2.1 million, 3.6 million wounded",
+        "participation": "Austria-Hungary was one of the main nations involved in the events of World War I.  In fact, the assassination of Austrian Archduke Franz Ferdinand in 1914 was the main short-term cause of the war and led to the events of the July Crisis.  As well, Austria-Hungary participated in some of the most significant battles of the war.  For example, during World War I, Austria-Hungary was one of the Central Powers alongside Germany, Bulgaria and the Ottoman Empire.  Austria-Hungary was an important contributor to the war on the Italian Front (also known as the Alpine Front), and in Balkans."
+    }
+}
+nation = 'France'
+print(nations["France"]["name"])
